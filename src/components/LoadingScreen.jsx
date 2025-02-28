c4af04739ecae06aec38fa045b265e865fcc3015
Daniel Chanda <danielchanda41@gmail.com>
2025-02-28 21:58:33 +0200

    Navbar creation

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,36 +1,11 @@
-import { useEffect, useState } from "react";
+import React from 'react'
 
-export const LoadingScreen = ({ onComplete }) => {
-  const [text, setText] = useState("");
-  const fullText = "<Hello Wolrd />";
-
-  useEffect(() => {
-    let index = 0;
-    const interval = setInterval(() => {
-      setText(fullText.substring(0, index));
-      index++;
-
-      if (index > fullText.length) {
-        clearInterval(interval);
-
-        setTimeout(() => {
-          onComplete();
-        }, 1000);
-      }
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, [onComplete]);
-
+const LoadingScreen = () => {
   return (
-    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold">
-        {text} <span className="animate-blink ml-1"> | </span>
+      <div className='flex flex-col items-center justify-center fixed inset-0 z-50 bg-black text-grey-100'>
+            <div className='mb-4 text-4xl font-mono font-bold'>Hello World</div>
       </div>
+  )
+}
 
-      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
-      </div>
-    </div>
-  );
-};
+export default LoadingScreen
